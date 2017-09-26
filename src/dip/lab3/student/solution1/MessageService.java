9f41c2183adf280f4613bf7bd039b62e18cb1aea
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-26 16:41:32 -0500

    Establish MessageService

--- src/dip/lab3/student/solution1/MessageService.java
+++ src/dip/lab3/student/solution1/MessageService.java
@@ -10,5 +10,16 @@ package dip.lab3.student.solution1;
  * @author Isaac
  */
 public class MessageService {
+    private Input input;
+    private Output output;
     
+    public MessageService(Input input, Output output){
+        this.input = input;
+        this.output = output;
+    }
+    
+    public void beginMessaging() {
+        String msg = input.receiveMessage();
+        output.outputMessage(msg);
+    }
 }
--- src/dip/lab3/student/solution1/Startup.java
+++ src/dip/lab3/student/solution1/Startup.java
@@ -11,6 +11,17 @@ package dip.lab3.student.solution1;
  */
 public class Startup {
     public static void main(String[] args) {
+        Input keyboardInput = new KeyboardInput();
+	Input fileInput = new FileInput();
+        Output consoleOut = new ConsoleOutput();
+        Output guiOut = new GUIOutput();
+		
+	MessageService msgService1 = new MessageService(keyboardInput, consoleOut);
+	msgService1.beginMessaging();
         
+        System.out.println("");
+        
+        MessageService msgService2 = new MessageService(fileInput, guiOut);
+	msgService2.beginMessaging();
     }
 }
